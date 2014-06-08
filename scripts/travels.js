d5f3da5e6b4e976808ff72b89f6f034524825c9f
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-06-08 22:23:34 +0100

    travels ordered by title

--- scripts/travels.js
+++ scripts/travels.js
@@ -4,6 +4,7 @@ var travelsApp = angular.module('travelsApp', []);
 	travelsApp.controller('TravelsListCtrl', function($scope, $http){
 		$http.get('data/travels.json').success(function(data) {
 			$scope.travels = data;
+			$scope.orderProp = 'title';
 		});
 	});
 
