545ba951b3590b3934a71f786433e1ae81a20713
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-06-08 22:24:12 +0100

    4 random pics fetched as random pictures

--- scripts/travels.js
+++ scripts/travels.js
@@ -3,6 +3,17 @@ var travelsApp = angular.module('travelsApp', []);
 
 	travelsApp.controller('TravelsListCtrl', function($scope, $http){
 		$http.get('data/travels.json').success(function(data) {
+			var t, pics;
+			for (t in data) {
+				data[t].previewPics = data[t].pics
+					// duplicate initial pics list
+					.slice(0)
+					// randomize
+					.sort(function() { return 0.5 - Math.random() })
+					// takes the 4 first
+					.splice(0, 4);
+			}
+
 			$scope.travels = data;
 			$scope.orderProp = 'title';
 		});
