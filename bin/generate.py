0847c9d669c6e0d3871a20028fdff86a1fe6ca28
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-06-09 14:33:31 +0100

    generation of the JSON file implemented

--- bin/generate.py
+++ bin/generate.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import json
 from os.path import isfile, join
 
 def getFiles(path):
@@ -13,11 +14,48 @@ def getFiles(path):
 
 	return fileList
 
+def generate(path):
+	arbo = {}
+	for f in getFiles(path):
+		f = f[len(path):]
+		struct = f.split('/')
+		fileName = struct[-1]
+		place = None
+		try:
+			travel = struct[-3]
+			place = struct[-2]
+			placeId = ''.join(place.split(' '))
+		except IndexError:
+			travel = struct[-2]
+
+		travelId = ''.join(travel.split(' '))
+
+		if travelId not in arbo.keys():
+			arbo[travelId] = {
+				"title": travel,
+				"places": [],
+				"pics": []
+			}
+
+		if place is None:
+			arbo[travelId]["pics"].append(fileName)
+		else:
+			if place not in arbo[travelId]["places"]:
+				arbo[travelId]["places"].append(place)
+
+			placeIndex = arbo[travelId]["places"].index(place)
+			if len(arbo[travelId]["pics"]) < placeIndex + 1:
+				arbo[travelId]["pics"].append([])
+
+			arbo[travelId]["pics"][placeIndex].append(fileName)
+	return arbo
+
 def main(argv):
 	path = argv[0]
-
-	fileList = getFiles(path)
-	print fileList
+	destFile = argv[1]
+	arbo = generate(path)
+	with open(destFile, 'w') as outfile:
+		json.dump(arbo, outfile)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
