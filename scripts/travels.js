585a5afe701fc35e07681010249edb867023c1c7
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-06-09 13:38:49 +0100

    travels fetched once in the application lifetime

--- scripts/travels.js
+++ scripts/travels.js
@@ -1,22 +1,35 @@
 (function(){
-var travelsApp = angular.module('travelsApp', []);
+	var travelsApp = angular.module('travelsApp', []),
+		travels,
+		getTravels;
+
+	getTravels = function($scope, $http)
+	{
+		if (travels != null) {
+			$scope.travels = travels;
+		}
+		else {
+			$http.get('data/travels.json').success(function(data) {
+				var t, pics;
+				for (t in data) {
+					data[t].previewPics = data[t].pics
+						// duplicate initial pics list
+						.slice(0)
+						// randomize
+						.sort(function() { return 0.5 - Math.random() })
+						// takes the 4 first
+						.splice(0, 4);
+				}
+
+				$scope.travels = data;
+				$scope.orderProp = 'title';
+			});
+		}
+	};
+
 
 	travelsApp.controller('TravelsListCtrl', function($scope, $http){
-		$http.get('data/travels.json').success(function(data) {
-			var t, pics;
-			for (t in data) {
-				data[t].previewPics = data[t].pics
-					// duplicate initial pics list
-					.slice(0)
-					// randomize
-					.sort(function() { return 0.5 - Math.random() })
-					// takes the 4 first
-					.splice(0, 4);
-			}
-
-			$scope.travels = data;
-			$scope.orderProp = 'title';
-		});
+		getTravels($scope, $http);
 	});
 
 	window.travelsApp = travelsApp;
